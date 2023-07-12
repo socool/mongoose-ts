ee47a2f4f5fea29c35e6aaf16306d296fd78ba82
Kamolsit Mongkolsrisawat <kamolsit@computerlogy.com>
2023-07-12 16:51:19 +0700

    connect mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import { json } from 'body-parser';
 import { todoRouter } from './routes/todo';
+import mongoose from 'mongoose';
 
 const app = express()
 app.use(json())
 app.use(todoRouter)
+
+require('dotenv').config()
+
+let mongo_url:string = process.env.MONGODB_URL!;
+mongoose.connect(mongo_url).then(() => console.log('Mongodb Connected'))
 
 app.listen(3000,() => {
     console.log('server is listening on port 3000')
