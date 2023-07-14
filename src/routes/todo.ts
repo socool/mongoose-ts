8227a8c25b39db1092d28d69aac1a52567f96723
Kamolsit Mongkolsrisawat <kamolsit@computerlogy.com>
2023-07-14 08:23:35 +0000

    implement POST

--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -1,4 +1,5 @@
 import express, {Request, Response} from 'express'
+import { Todo } from '../models/todo'
 
 const router = express.Router()
 
@@ -7,7 +8,11 @@ router.get('/api/todo', [], (req: Request, res: Response) => {
 })
 
 router.post('/api/todo', async (req: Request ,res: Response) => {
-    return res.send('new todo created')
+    const {title, description} = req.body;
+
+    const todo = Todo.build({title, description})
+    await todo.save()
+    return res.status(201).send(todo)
 })
 
 export { router as todoRouter}
